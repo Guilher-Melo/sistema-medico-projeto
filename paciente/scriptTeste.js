cda585d771495908b78e22112085ecdb0e9370bd
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2023-12-03 18:38:57 -0300

    Adicionando disponibildade

--- paciente/scriptTeste.js
+++ paciente/scriptTeste.js
@@ -5,17 +5,12 @@ function marcarConsulta() {
     if (selectedMedico) {
         var idMedico = selectedMedico.value;
 
-        // Depure os valores para verificar se são os esperados
-        console.log('idMedico:', idMedico);
-
-        // Tente obter o elemento
         var selectHorario = document.getElementById('horarios_' + idMedico);
 
         if (selectHorario) {
             var selectedHorario = selectHorario.value;
 
             if (selectedHorario) {
-                // Redirecione para a página com os parâmetros na URL
                 window.location.href = 'marcar_consulta.php?id_medico=' + idMedico + '&horario=' + encodeURIComponent(selectedHorario);
             } else {
                 alert('Por favor, selecione um horário antes de marcar a consulta.');
